406a2156b67e9c04bd1fb4c0ffa46e2e266954f2
Vivian Chen <vivianindatub@gmail.com>
2021-11-12 11:03:00 -0800

    quest prefab

---
+++ Assets/Tony/Quest/QuestUIItem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class QuestUIItem : MonoBehaviour //attach to each quest prefab slot
+{
+    [SerializeField]
+    private TMP_Text questName, questProgress;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
